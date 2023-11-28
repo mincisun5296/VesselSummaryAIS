f5e760622798ac95aa091302fc966fc96df51e05
MinCi Sun <mincisun5296@tamu.edu>
2023-11-28 12:25:48 -0600

    The AIS data loading problem is fixed (I need to install my package again using devtools::install_github("mincisun5296/VesselSummaryAIS")

--- R/data.R
+++ R/data.R
@@ -1,9 +1,9 @@
-#' AISdata
+#' Description of AISdata
 #'
 #' The AISdata is created through the real AIS data of 2022-01-03 to serve as an example. Since 2012, the U.S. Coast Guard has collected dynamic vessel traffic data, or Automatic Identification System (AIS) data, to ensure the safety of vessel travels. The data can be downloaded from https://marinecadastre.gov/ais/ , and it contains 17 variables for each signal. This AISdata extract the first 99999 rows of information for testing.
 
-
-#'@format A data frame with 99999 rows and 17 variables. However, please note that in the VesselSummaryAIS package, only variables of MMSI, BaseDataTime, LAT, LON, SOG, VesselType, Length, and Width are used.
+#' @name AISdata
+#' @format A data frame with 99999 rows and 17 variables. However, please note that in the VesselSummaryAIS package, only variables of MMSI, BaseDataTime, LAT, LON, SOG, VesselType, Length, and Width are used.
 #' \describe{
 #' \item{MMSI}{Maritime Mobile Identity value, which is an ID of a vessel.}
 #' \item{BaseDataTime}{The UTC data and time that location signal is received}
@@ -26,8 +26,6 @@
 
 #' @export
 #' @source {Created through the AIS data of 2022-01-03 to serve as an example}
-#' @examples data(AISdata)
 
-
-AISdata <- read.csv("test.csv", header = T)
+"AISdata"
 
