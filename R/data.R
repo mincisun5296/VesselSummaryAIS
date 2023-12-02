470d3eaf755f94fc4d11f005f760eddf078fc8a1
MinCi Sun <mincisun5296@tamu.edu>
2023-12-01 22:48:27 -0600

    change the default range of coordinates

--- R/data.R
+++ R/data.R
@@ -3,7 +3,7 @@
 #' The AISdata is created through the real AIS data of 2022-01-03 to serve as an example. Since 2012, the U.S. Coast Guard has collected dynamic vessel traffic data, or Automatic Identification System (AIS) data, to ensure the safety of vessel travels. The data can be downloaded from https://marinecadastre.gov/ais/ , and it contains 17 variables for each signal. This AISdata extract the first 99999 rows of information for testing.
 
 #' @name AISdata
-#' @format A data frame with 99999 rows and 17 variables. However, please note that in the VesselSummaryAIS package, only variables of MMSI, BaseDataTime, LAT, LON, SOG, VesselType, Length, and Width are used.
+#' @format A data frame with 99999 rows and 17 variables. In the VesselSummaryAIS package, only variables of MMSI, BaseDataTime, LAT, LON, SOG, VesselType, Length, and Width are used.
 #' \describe{
 #' \item{MMSI}{Maritime Mobile Identity value, which is an ID of a vessel.}
 #' \item{BaseDataTime}{The UTC data and time that location signal is received}
@@ -15,7 +15,7 @@
 #' \item{VesselName}{Name as shown on the station radio license }
 #' \item{IMO}{International Maritime Organization Vessel number}
 #' \item{CallSign}{Call sign as assigned by Federal Communications Commission}
-#' \item{VesselType}{Vessel type as defined in NAIS specifications. You can use classify(VesselType) to get the ship type}
+#' \item{VesselType}{The value can represent 9 ship types, including Cargo, Fishing, Military, Not_available, Others, Passengers, Pleasure, Tanker, and Tugboat. Please see this document for detailed categorization: https://coast.noaa.gov/data/marinecadastre/ais/VesselTypeCodes2018.pd}
 #' \item{Status}{Navigation status as defined by the COLREGS}
 #' \item{Length}{Length of vessel in meters}
 #' \item{Width}{Width of vessel in meters}
